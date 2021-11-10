8d738c748c6914881820f7fda351dffaae8bace8
SyntacticalAnomaly <thisguycalledk@gmail.com>
2021-11-10 14:09:03 -0800

    Drafted attempt at solving problem 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+  let inputLength = inputArray.length;
+  let argsLength = args.length;
+  for (let i=0;i<inputLength;i++) {
+    for (let j=0;j<argsLength;j++) {
+      if (inputArray[i] === args[j]) {
+        inputArray.splice(i, 1);
+      }
+    }
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
